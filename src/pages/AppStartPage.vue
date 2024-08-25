ae8a7c54d5c1a875dd9852f0a9663c4f26f482fe
Anatoliy Zakhryapin <anatoli.zakhryapin@gmail.com>
2024-08-25 12:06:42 +0200

    ADD StartPage

---
+++ src/pages/AppStartPage.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class="start-page">
+        <div class="start-page_content">
+            <img class="logo" src="../assets/logo.svg" alt="Logo">
+            <button class="btn-custom">
+               Start
+            </button>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+@import '../styles/mixins.scss';
+
+.start-page {
+    @include background-base;
+    @include flex-column-center;
+
+    height: 100vh;
+
+    .start-page_content {
+        @include flex-column-center;
+
+        .logo {
+            width: 60%;
+            min-width: 250px;
+            max-width: 700px;
+        }
+        
+        .btn-custom {
+            @include btn-custom;
+            font-size: 32px;
+        }
+    }
+
+}
+</style>
--- src/router.js
+++ src/router.js
@@ -1,14 +1,20 @@
 import { createRouter, createWebHistory } from 'vue-router';
 
 import AppHome from './pages/AppHome.vue';
+import AppStartPage from './pages/AppStartPage.vue';
 
 const router = createRouter({
     history: createWebHistory(),
     routes: [
         {
-            path: '/',
+            path: '/home',
             name: 'home',
             component: AppHome,
+        },
+        {
+            path: '/',
+            name: 'start',
+            component: AppStartPage,
         },
     ]
 })
