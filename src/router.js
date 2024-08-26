1d3a2c4607175049a7ef6eb992bf56e785e77826
Anatoliy Zakhryapin <anatoli.zakhryapin@gmail.com>
2024-08-26 16:17:08 +0200

    ADD new routes and add requireAuth

--- src/router.js
+++ src/router.js
@@ -1,10 +1,14 @@
 import { createRouter, createWebHistory } from 'vue-router';
 
+import { storeAuth } from '../src/store/storeAuth';
 import AppHome from './pages/AppHome.vue';
 import AppStartPage from './pages/StartPage/AppStartPage.vue';
 import AppIntro1 from './pages/StartPage/AppIntro1.vue';
 import AppIntro2 from './pages/StartPage/AppIntro2.vue';
 import AppIntro3 from './pages/StartPage/AppIntro3.vue';
+import AppRegister from './pages/Auth/AppRegister.vue';
+import AppLogin from './pages/Auth/AppLogin.vue';
+import AppDashboard from './pages/AppDashboard.vue';
 
 const router = createRouter({
     history: createWebHistory(),
@@ -34,7 +38,31 @@ const router = createRouter({
             name: 'intro-3',
             component: AppIntro3,
         },
+        {
+            path: '/register',
+            name: 'register',
+            component: AppRegister,
+        },
+        {
+            path: '/login',
+            name: 'login',
+            component: AppLogin,
+        },
+        {
+            path: '/dashboard',
+            name: 'dashboard',
+            component: AppDashboard,
+            meta: { requiresAuth: true }
+        },
     ]
-})
+});
+
+router.beforeEach((to, from, next) => {
+    if (to.meta.requiresAuth && !storeAuth.isAuthenticated) {
+        next('/');
+    } else {
+        next();
+    }
+});
 
 export {router};
