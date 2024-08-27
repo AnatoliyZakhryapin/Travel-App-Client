03c6ea5fcd12377b197c4ed35f6cc82c9b8e421f
Anatoliy Zakhryapin <anatoli.zakhryapin@gmail.com>
2024-08-27 10:14:38 +0200

    ADD mobile nav bar to Dashboard

--- src/main.js
+++ src/main.js
@@ -13,12 +13,12 @@ import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
 /* import specific icons */
-import {} from '@fortawesome/free-solid-svg-icons';
-import {} from '@fortawesome/free-regular-svg-icons';
-import {} from '@fortawesome/free-brands-svg-icons';
+import { faHouse, faSuitcaseRolling, faHeart, faRightFromBracket } from '@fortawesome/free-solid-svg-icons';
+// import { farHeart } from '@fortawesome/free-regular-svg-icons';
+// import { fabGithub } from '@fortawesome/free-brands-svg-icons';
 
 /* add icons to the library */
-library.add();
+library.add( faHouse, faSuitcaseRolling, faHeart, faRightFromBracket );
 
 import App from './App.vue';
 import { storeAuth } from '../src/store/storeAuth';
--- src/pages/AppDashboard.vue
+++ src/pages/AppDashboard.vue
@@ -1,6 +1,28 @@
 <template>
-    <div>
-        <h1>DACHBOARD</h1>
+    <div class="dashboard-page">
+        <main>
+            <h1>DACHBOARD</h1>
+        </main>
+        <header>
+            <BNav class="nav-mobile">
+                <BNavItem active>
+                    <font-awesome-icon :icon="['fas', 'house']" />
+                    Home
+                </BNavItem>
+                <BNavItem>
+                    <font-awesome-icon :icon="['fas', 'suitcase-rolling']" />
+                    Trip
+                </BNavItem>
+                <BNavItem>
+                    <font-awesome-icon :icon="['fas', 'heart']" />
+                    Favorit
+                </BNavItem>
+                <BNavItem>
+                    <font-awesome-icon :icon="['fas', 'right-from-bracket']" />
+                    Logout
+                </BNavItem>
+            </BNav>
+        </header>
     </div>
 </template>
 
@@ -10,5 +32,26 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+@import '../styles/mixins.scss';
+@import '../styles/variables.scss';
 
+    .dashboard-page {
+        height: 100vh;
+        background-color: $light-grey;
+
+        header {
+            border-top-left-radius: 40px;
+            border-top-right-radius: 40px;
+            background-color: white;
+            padding: 10px 0px;
+            position: fixed;
+            bottom: 0;
+            width: 100%;
+            justify-content: center;
+            display: flex;
+
+
+
+        }
+    }
 </style>
