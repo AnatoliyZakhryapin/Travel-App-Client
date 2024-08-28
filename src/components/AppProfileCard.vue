357b3c6159618089b01fa42746d260544702acd6
Anatoliy Zakhryapin <anatoli.zakhryapin@gmail.com>
2024-08-28 23:52:18 +0200

    ADD component Profile Card

---
+++ src/components/AppProfileCard.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="card-profile d-flex justify-content-between">
+        <div class="title">
+            <h3>Good monring</h3>
+            <h1>User Name</h1>
+        </div>
+        <div class="avatar">
+            <BAvatar />
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    setup() {
+
+
+        return {}
+    }
+}
+</script>
+
+<style lang="scss" scoped></style>
